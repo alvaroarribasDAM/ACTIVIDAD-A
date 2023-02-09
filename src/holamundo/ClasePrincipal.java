a950b0f745554eba4ebcb8bc13647c2f55b4838b
dam <dam@203P20>
2023-02-09 10:09:49 +0100

    Proyecto creado por el dueño.

---
+++ src/holamundo/ClasePrincipal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author dam
+ */
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        System.out.println("Clase creada por el dueño del repositorio.");
+    }
+}
